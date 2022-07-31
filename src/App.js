59153cc6a65fc152283fff1967b6c770fa188975
Abhinav singh <abhinasingh2001@gmail.com>
2022-07-31 16:06:33 +0530

    bmi

--- src/App.js
+++ src/App.js
@@ -1,49 +1,56 @@
 import "./App.css";
 import React, { useState } from "react";
 
-
-
 function App() {
-  const [user, setUser] = useState({
-    height: "",
-    weight: "",
-    
-  });
-  let name , value;
-
-  const handleInputs = (e) => {
-    // console.log(e);
-    name = e.target.name;
-    value = e.target.value;
-    setUser({ ...user, [name]: value });
-  };
-
+  const [bmi, setBmi] = useState();
+  const [info, setInfo] = useState();
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
   
 
+ 
+  let output;
+  const handleBmi = () => {
+    let val = (
+      [Number(weight) / Number(height) / Number(height)] * 10000
+    ).toFixed(1);
+    setBmi(val);
+    if (val < 18.5) {
+      setInfo("Under Weight");
+    } else if (val > 18.5 && val <= 24.9) {
+      setInfo("Healthy");
+    } else if (val > 24.9 && val < 30) {
+      setInfo("Overweight");
+    } else {
+      setInfo("Obese");
+    }
+  };
+
   return (
-  <div>
-    <h1>BMI Calculator</h1>
-    <form>
-       <input
-              type="number"
-              className="input"
-              placeholder="weight*(in meter)"
-              name="weight"
-              value={user.weight}
-              onChange={handleInputs}
-            ></input>
+    <div>
+      <h1>BMI Calculator</h1>
+      <form>
         <input
-              type="number"
-              className="input"
-              placeholder="height*"
-              name="height"
-              value={user.height}
-              onChange={handleInputs}
-            ></input>
-    </form>
-    <button>Submit</button>
+          type="number"
+          className="input"
+          placeholder="weight*(in meter)"
+          name="weight"
+          
+          onChange={(e) => setHeight(e.target.value)}
+        ></input>
+        <input
+          type="number"
+          className="input"
+          placeholder="height*"
+          name="height"
+          
+          onChange={(e) => setWeight(e.target.value)}
+        ></input>
+      </form>
+      <button onClick={handleBmi}>Submit</button>
+      <h1>{bmi}</h1>
+      <h2>{info}</h2>
     </div>
-    
   );
 }
 
