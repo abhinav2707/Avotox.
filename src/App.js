f1948ef984ee032c332b490729171eafc54d5d73
Kushagra <kushagra3288@gmail.com>
2022-07-31 14:33:42 +0530

    bmi calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Input, VStack, Button, Text } from "@chakra-ui/react";
 
 function App() {
+  const [formData, setFormData] = useState({
+    height: 0,
+    weight: 0,
+  });
+  const [error, setError] = useState({
+    height: "",
+    weight: "",
+  });
+  const [result, SetResult] = useState(0);
+
+  const calculate = (e) => {
+    e.preventDefault();
+
+    if (formData.height === 0) {
+      setError({ ...error, height: "height can't be zero" });
+      return false;
+    }
+    setError({ ...error, height: "" });
+
+    if (formData.weight === 0) {
+      setError({ ...error, weight: "weight can't be zero" });
+      return false;
+    }
+    setError({ ...error, weight: "" });
+
+    const bmi = formData.weight / (formData.height * formData.height);
+    SetResult(bmi);
+  };
+
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={10} spacing="30px" align="center">
+      <Text fontSize="3xl">BMI Calculator</Text>
+      <form onSubmit={calculate}>
+        <Input required m="20px 0 10px" type="number" width="md" name="height" onChange={changeHandler} placeholder="Height(m)" value={formData.height} />
+        <p>{error.height}</p>
+
+        <Input required m="20px 0 10px" type="number" width="md" name="weight" onChange={changeHandler} placeholder="Weight(Kg)" value={formData.weight} />
+        <p>{error.weight}</p>
+
+        <Button m="20px 0" align="center" colorScheme="blue" variant="solid" type="submit">
+          Calculate
+        </Button>
+      </form>
+
+      {result !== 0 ? <Text fontSize="4xl">BMI: {result}</Text> : null}
+    </VStack>
   );
 }
 
